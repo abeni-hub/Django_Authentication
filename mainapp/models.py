82b3099ae32d461b690d8c217f8c3e48ffa0381c
Abenezer Sileshi <138670844+abeni-hub@users.noreply.github.com>
2024-08-17 22:54:36 +0300

    Update models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,9 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
