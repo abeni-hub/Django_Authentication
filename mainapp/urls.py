19563da7f1136c02460ba994d40da4182f0a58f4
Abenezer Sileshi <138670844+abeni-hub@users.noreply.github.com>
2024-08-17 22:55:47 +0300

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -20,5 +20,10 @@ urlpatterns = [
     
     #user registration
     path('register/',views.register ,name='register'),
+    path('', views.item_list, name='item_list'),
+    path('item/<int:pk>/', views.item_detail, name='item_detail'),
+    path('item/new/', views.item_create, name='item_create'),
+    path('item/<int:pk>/edit/', views.item_update, name='item_update'),
+    path('item/<int:pk>/delete/', views.item_delete, name='item_delete'),
 
 ]
